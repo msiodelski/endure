a0b8adc412a451fd5b5a0920b99836dc85e9aec8
Marcin Siodelski <msiodelski@gmail.com>
2023-03-27 21:34:32 +0200

    [#7] Added program arguments
    Added support for the collect command that runs a traffic capture on the
    specified interface. Otherwise, the program displays help. Also, removed
    the dependency on the graceful crate.

--- src/main.rs
+++ src/main.rs
@@ -2,37 +2,14 @@
 
 //! endure is a DHCP diagnostics tool.
 
-use std::{sync::atomic::Ordering, thread};
-use graceful::SignalGuard;
-use listener::Filter;
+use clap::Parser;
+use cli::Cli;
 
-pub mod listener;
+pub mod cli;
 pub mod dispatcher;
+pub mod listener;
 
 fn main() {
-    // Block the signals.
-    let signal_guard = SignalGuard::new();
-
-    // Run the dispatcher thread.
-    let handle = thread::spawn(|| {
-        let mut dispatcher = dispatcher::Dispatcher::new();
-        let filter = Filter::new().bootp_server_relay();
-        dispatcher.add_listener("wlo1", filter).expect("listener already added");
-        dispatcher.dispatch();
-    });
-
-    // Wait for the signal to stop the dispatcher thread.
-    signal_guard.at_exit(move |sig| {
-        let signal_name: &str;
-        match sig {
-            2 => signal_name = "Ctrl+C",
-            3 => signal_name = "QUIT signal",
-            15 => signal_name = "SIGTERM signal",
-            _ => signal_name = "signal",
-        }
-        println!("received {}", signal_name);
-        println!("shutting down...");
-        dispatcher::STOP.store(true, Ordering::Release);
-        handle.join().unwrap();
-    });
+    let cli = Cli::parse();
+    cli.run();
 }
