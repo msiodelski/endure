f2edd20ef16c5d6eef80b14bcce25669d6d02ac2
Marcin Siodelski <msiodelski@gmail.com>
2024-03-04 23:32:38 +0100

    [#29] Enabled doc linters

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
-#![warn(missing_docs)]
-
 //! endure is a DHCP diagnostics tool.
+
+#![deny(missing_docs)]
+#![deny(rustdoc::broken_intra_doc_links)]
+#![deny(rustdoc::broken_intra_doc_links)]
+#![deny(rustdoc::private_intra_doc_links)]
+#![deny(rustdoc::missing_crate_level_docs)]
+#![deny(rustdoc::invalid_codeblock_attributes)]
+#![deny(rustdoc::invalid_html_tags)]
+#![deny(rustdoc::invalid_rust_codeblocks)]
+#![deny(rustdoc::bare_urls)]
+#![deny(rustdoc::unescaped_backticks)]
+#![deny(rustdoc::redundant_explicit_links)]
 
 use clap::Parser;
 use cli::Cli;
