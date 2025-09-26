dbe07a767a75339bc210afc35fc5f137665e3906
msiodelski <msiodelski@users.noreply.github.com>
2025-09-26 09:30:56 +0000

    deploy: e7bed35d85256ad9a6653c6105cc158c473b1787

--- src-files.js
+++ src-files.js
@@ -1,3 +1,2 @@
-var srcIndex = new Map(JSON.parse('[["endure",["",[["auditor",[],["common.rs","conversation.rs","metric.rs","mod.rs","opcode.rs","packet_time.rs","retransmission.rs","roundtrip.rs","spectrum.rs","util.rs"]],["proto",[["dhcp",[],["mod.rs","v4.rs"]]],["bootp.rs","buffer.rs","mod.rs"]]],["analyzer.rs","cli.rs","dispatcher.rs","main.rs","pcap_processor.rs","sse.rs"]]]]'));
-createSrcSidebar();
-//{"start":36,"fragment_lengths":[330]}
+createSrcSidebar('[["endure",["",[["auditor",[],["common.rs","conversation.rs","metric.rs","mod.rs","opcode.rs","packet_time.rs","retransmission.rs","roundtrip.rs","spectrum.rs","util.rs"]],["proto",[["dhcp",[],["mod.rs","v4.rs"]]],["bootp.rs","buffer.rs","mod.rs"]]],["analyzer.rs","cli.rs","dispatcher.rs","main.rs","pcap_processor.rs","sse.rs"]]]]');
+//{"start":19,"fragment_lengths":[330]}
--- trait.impl/core/marker/trait.Copy.js
+++ trait.impl/core/marker/trait.Copy.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["endure",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.1/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"endure/proto/bootp/enum.HType.html\" title=\"enum endure::proto::bootp::HType\">HType</a>"]]]]);
+    var implementors = Object.fromEntries([["endure",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"endure/proto/bootp/enum.HType.html\" title=\"enum endure::proto::bootp::HType\">HType</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
